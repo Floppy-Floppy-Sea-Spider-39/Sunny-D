4b9477df53a304bde5d691e00e21565dece4696a
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-03-29 15:32:28 -0400

    Geolocation is working, moved weather API request into .then chain of radar API request

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -13,22 +13,58 @@ function Home() {
   const [condition, updateCondition] = useState("");
   const [city, updateCity] = useState("");
   const [region, updateRegion] = useState("");
+  const [zipcode, updateZipcode] = useState(0)
+
+  function showPosition(position){
+    console.log("COORDINATES ----> ",position.coords.latitude, position.coords.longitude)
+    fetch(`https://api.radar.io/v1/geocode/reverse?coordinates=${position.coords.latitude}%2C${position.coords.longitude}`, {
+      headers: {
+          'Authorization': 'prj_live_pk_058442fcb54e57751c139cdb5d5b1538755c7a39'
+      }
+  })
+  
+      .then(res => res.json())
+      .then(res => {
+        console.log("GEOLOCATION API RESPONSE --->", res.addresses[0].postalCode)
+        fetch(
+          `http://api.weatherapi.com/v1/current.json?key=3b98cf2d582f413d83c172329232503&q=${res.addresses[0].postalCode}`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            updateTemp(res.current.temp_f);
+            updateUv(res.current.uv);
+            updateCondition(res.current.condition.icon);
+            updateCity(res.location.name);
+            updateRegion(res.location.region);
+          })
+          .catch((err) => {
+            console.log("Error in weather api call: ", err);
+          });
+      })
+      
+      .catch(err => {
+        console.log("error in Geolocation API call", err)
+      })
+  }
+
 
   useEffect(() => {
-    fetch(
-      `http://api.weatherapi.com/v1/current.json?key=3b98cf2d582f413d83c172329232503&q=${location.state.zipcode}`
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        updateTemp(res.current.temp_f);
-        updateUv(res.current.uv);
-        updateCondition(res.current.condition.icon);
-        updateCity(res.location.name);
-        updateRegion(res.location.region);
-      })
-      .catch((err) => {
-        console.log("Error in weather api call: ", err);
-      });
+    navigator.geolocation.getCurrentPosition(showPosition)
+    
+    // fetch(
+    //   `http://api.weatherapi.com/v1/current.json?key=3b98cf2d582f413d83c172329232503&q=${zipcode}`
+    // )
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     updateTemp(res.current.temp_f);
+    //     updateUv(res.current.uv);
+    //     updateCondition(res.current.condition.icon);
+    //     updateCity(res.location.name);
+    //     updateRegion(res.location.region);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Error in weather api call: ", err);
+    //   });
   });
 
   return (
